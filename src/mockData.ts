8e4b870e96d8ffde8a5a77dd75a2a02196555b0d
hansbkhi <biggynegro12345@gmail.com>
2024-12-14 16:07:42 +0100

    Make Prisma optional for development and add mock data

---
+++ src/mockData.ts
@@ -0,0 +1,22 @@
+export const mockData = {
+  products: [
+    {
+      id: 1,
+      name: 'Sample Product 1',
+      price: 99.99,
+      description: 'This is a sample product description',
+      image: 'https://via.placeholder.com/150'
+    },
+    {
+      id: 2,
+      name: 'Sample Product 2',
+      price: 149.99,
+      description: 'Another sample product description',
+      image: 'https://via.placeholder.com/150'
+    }
+  ],
+  orders: [],
+  users: []
+};
+
+export default mockData;
